c33076b11f69cd4201d72da3f5bcfdeceb50d776
Josh <aftershock840@gmail.com>
2024-12-22 09:56:26 -0500

    added experimental javascript to js file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-// Prevent us from attempting to use variables
-// that are not declared
+//Prevent us from attempting to use variables
+//that are not declared
 "use strict"
 
+/* Even and odd function
+function evenOrOdd(num) {
+    if(num % 2 === 0){
+        console.log(`This is an even number!`);
+    }else{
+        console.log(`This is an odd number!`);
+        };
+};
+
+checking to see how accurate it solves an arithmatic problem with multiple operators.
+
+console.log(10 - 5 / 5 + 2 * 5 % 4);
+*/
